d693b6df1040cbb35db309ac4d8ca2ca20373b8c
ashmitdhondiyal <ashmitdhondiyal2005@gmail.com>
2025-07-12 13:50:06 +0530

    added JavaScript file

---
+++ script.js
@@ -0,0 +1,40 @@
+var start = new Date().getTime();
+var box = document.getElementById("box");
+
+//color changing of the box
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+//moving of the box
+function move() {
+    var left;
+    var top;
+    var wh;
+    left = Math.random() * 300;
+    top = Math.random() * 300;
+    wh = ((Math.random() * 400) + 100);
+    box.style.left = left + "px";
+    box.style.top = top + "px";
+    box.style.width = wh + "px";
+    box.style.height = wh + "px";
+    box.style.display = "block";
+    box.style.backgroundColor = getRandomColor();
+    start = new Date().getTime();
+
+}
+
+//main of the program
+move();
+box.onclick = function () {
+    box.style.display = "none";
+    var end = new Date().getTime();
+    var timetook = (end - start) / 1000;
+    alert(timetook);
+    move();
+}
